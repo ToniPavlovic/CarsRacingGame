7afaacabdee684283fe6017a49dc39f5a8afe50a
tonipvlc <tonipavlovic518@gmail.com>
2025-06-25 23:18:43 +0200

    chore: commit of car class because the changes were not commited

--- src/Car.java
+++ src/Car.java
@@ -2,24 +2,29 @@ public class Car {
     private final String manufacturer;
     private final String model;
     private final int year;
-    private final int horsepower;
-    private final int weight;
     private final int topSpeed;
     private final double acceleration;
 
-    public Car(String manufacturer, String model, int year, int horsepower, int weight, int topSpeed, double acceleration){
+    public Car(String manufacturer, String model, int year, int topSpeed, double acceleration){
         this.manufacturer = manufacturer;
         this.model = model;
         this.year = year;
-        this.horsepower = horsepower;
-        this.weight = weight;
         this.topSpeed = topSpeed;
         this.acceleration = acceleration;
     }
 
-    public double getPerformanceScore(){
-        return (horsepower * 1.5 + topSpeed * 2) - (weight * 0.5 + acceleration * 30);
+    public String getModel(){
+        return model;
     }
+
+    public int getTopSpeed(){
+        return topSpeed;
+    }
+
+    public double getAcceleration(){
+        return acceleration;
+    }
+
 
     public String toString() {
         return manufacturer + " " +model + " (" + year + ")";
